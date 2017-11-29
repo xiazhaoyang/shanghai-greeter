987014b698cbc570840b5723f4b872c3cf36a9b8
Ryan Kretch <rkretch91@gmail.com>
2017-11-29 14:43:46 +0800

    added expectations columns to experiences

--- db/migrate/20171128065332_add_column_to_experiences.rb
+++ db/migrate/20171128065332_add_column_to_experiences.rb
@@ -2,5 +2,8 @@ class AddColumnToExperiences < ActiveRecord::Migration[5.1]
   def change
     add_column :experiences, :name, :string
     add_column :experiences, :description, :text
+    add_column :experiences, :expectation_one, :string
+    add_column :experiences, :expectation_two, :string
+    add_column :experiences, :expectation_three, :string
   end
 end
