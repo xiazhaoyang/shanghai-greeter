a6ee7507d56e473a654978bf3987e9260b45f9b5
Audrey Gourdji <agourdji@gmail.com>
2017-12-06 20:59:48 +0800

    edited the applications controller to allow viewing about us and greeter page without signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :greeter, :about]
 end
