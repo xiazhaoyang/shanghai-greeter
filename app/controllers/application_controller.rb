feca275e419770120cfb52a8dd674c8918cd96a4
Audrey Gourdji <agourdji@gmail.com>
2017-12-07 12:34:35 +0800

    added greeter settings to account settings form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: [:index, :greeter, :about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+     # For additional in app/views/devise/registrations/edit.html.erb
+     devise_parameter_sanitizer.permit(:account_update, keys: [:phone_number, :wechat_id, :language])
+   end
 end
